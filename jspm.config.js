074ed5c56e70bee85595b8f75669de89dd77803e
Yoctopuce <dev@yoctopuce.com>
2016-04-11 14:09:19 +0200

    "New build based on revison 23857"

--- jspm.config.js
+++ jspm.config.js
@@ -1,10 +1,4 @@
 SystemJS.config({
-  packageConfigPaths: [
-    "npm:@*/*.json",
-    "npm:*.json",
-    "github:*/*.json"
-  ],
-  globalEvaluationScope: false,
   transpiler: "plugin-babel",
   babelOptions: {
     "stage": 3,
@@ -14,14 +8,18 @@ SystemJS.config({
       "runtime",
       "optimisation.modules.system"
     ]
-  },
+  }
+});
 
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
   map: {
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
-    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
-    "path": "github:jspm/nodelibs-path@0.2.0-alpha",
-    "plugin-babel": "npm:systemjs-plugin-babel@0.0.2",
-    "process": "github:jspm/nodelibs-process@0.2.0-alpha"
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.9"
   }
 });
